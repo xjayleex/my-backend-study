03ea1ba2205dfdff14ed77299b5398519d7b4ea5
xjayleex <aprilmagnificat@gmail.com>
2020-09-30 17:15:59 +0900

    deploy

--- grpc_study/image_transfer/main.go
+++ grpc_study/image_transfer/main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	clitool "gopkg.in/urfave/cli.v2"
+	"github.com/xjayleex/my-backend-study/grpc_study/image_transfer/cli"
+	"os"
 )
 func main() {
 	app := &clitool.App{
 		Name : "imgtransfer",
 		Usage: "send a image",
 		Commands: []*clitool.Command{
-			&
+			&cli.Serve,
+			&cli.Send,
+		},
+		Flags: []clitool.Flag{
+			&clitool.BoolFlag{
+				Name: "debug",
+				Usage: "enables debug logging",
+			},
 		},
 	}
+
+	app.Run(os.Args)
 }
