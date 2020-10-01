af875adf8d3044f60613acabbcf187726f473535
xjayleex <aprilmagnificat@gmail.com>
2020-10-01 20:09:09 +0900

    Added setnx method

--- database_study/redis-auth-with-jwt/rdsclient.go
+++ database_study/redis-auth-with-jwt/rdsclient.go
@@ -1,5 +1,7 @@
 package main
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/go-redis/redis"
 	"github.com/pkg/errors"
@@ -9,38 +11,84 @@ type RedisClient struct {
 	client		*redis.Client
 	address 	string
 	port		string
+	db			int
+	ctx 		context.Context
+}
+
+type RData struct {
+	Key string
+	Value *RValue
+}
+
+type RValue struct {
+	Password string
+	Mail	string
+}
+
+func (rd *RData) MarshalBinary() ([]byte, error) {
+	return json.Marshal(rd)
+}
+func (rv *RValue) MarshalBinary() ([]byte, error) {
+	return json.Marshal(rv)
 }
 
 type RedisClientOpts struct {
 	Address	string
 	Port	string
+	DB 		int
 }
 func NewRedisClient(opts *RedisClientOpts) (c *RedisClient, err error) {
-	if opts.Address == "" {
+	if c == nil {
+		c = &RedisClient{}
+	}
+	if c.ctx == nil {
+		c.ctx = context.Background()
+	}
+
+	if opts.Address == ""  {
 		err = errors.Errorf("Address Required")
 		return
 	}
 	if opts.Port == "" {
 		err = errors.Errorf("Port Required")
 	}
-	c.address = opts.Address
+	c.address, c.port, c.db = opts.Address, opts.Port, opts.DB
 	c.client = redis.NewClient(&redis.Options{
 		Addr: c.address + ":" + c.port,
+		DB: c.db,
 	})
 	return
 }
 
 
+func (rc *RedisClient) Setnx(data *RData) {
+	boolCmd := rc.client.SetNX(rc.ctx, data.Key, data.Value,0)
+	flag , err := boolCmd.Result()
+	trap(err)
+	if !flag {
+		fmt.Println("Key Exists Already.")
+	} else {
+		fmt.Println("Success on creating key.")
+	}
+
+}
+
+
 func main() {
-	c, err := NewRedisClient(&RedisClientOpts{
-		Address: "dn2",
+	rc, err := NewRedisClient(&RedisClientOpts{
+		Address: "localhost",
 		Port: "6379",
+		DB: 1,
 	})
 	trap(err)
-
-	pong, err := c.client.Ping().Result()
+	pong, err := rc.client.Ping(rc.ctx).Result()
 	trap(err)
-	fmt.Println(pong)
+	if pong != "PONG" {
+		fmt.Println("Not Connected.")
+		os.Exit(1)
+	}
+	rc.Setnx(&RData{"users:jaehyunlee",
+		&RValue{"password", "bigdata304@gmail.com" }})
 }
 
 func trap (err error) {
