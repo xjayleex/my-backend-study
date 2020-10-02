5d9d8abe128c14542149d09b2445dfbe57308402
xjayleex <aprilmagnificat@gmail.com>
2020-10-03 01:49:59 +0900

    httpserver-to-kafka

--- data-platforms/kafka-client/http_server.go
+++ data-platforms/kafka-client/http_server.go
@@ -10,17 +10,18 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 var (
 	addr		= flag.String("addr", "localhost:8080", "The address to bind to")
 	brokers 	= flag.String("brokers","","csv broker list")
 	verbose		= flag.Bool("verbose", false, "If Set true, use sarama logging")
-	certFile	= flag.String("certificate", "","")
-	keyFile		= flag.String("key file", "","")
-	caFile		= flag.String("ca", "","")
-	verifySsl	= flag.String("ssl certs", "","")
-	topic 		= flag.String("topic name", "simple", "Default Topic is `simple`")
+	//certFile	= flag.String("certificate", "","")
+	//keyFile		= flag.String("key file", "","")
+	//caFile		= flag.String("ca", "","")
+	//verifySsl	= flag.String("ssl certs", "","")
+	//topic 		= flag.String("topic name", "simple", "Default Topic is `simple`")
 )
 
 func main() {
@@ -38,14 +39,30 @@ func main() {
 	brokerList := strings.Split(*brokers, ",")
 
 	server := &Server{
-
+		DataCollector: newDataCollector(brokerList),
+		AccessLogProducer: newAccessLogProducer(brokerList),
 	}
+	defer func () {
+		if err := server.Close(); err != nil {
+			log.Println("Failed to close server", err)
+		}
+	}()
+	log.Fatal(server.Run(*addr))
 
 }
 
 type Server struct {
 	DataCollector		sarama.SyncProducer
 	AccessLogProducer	sarama.AsyncProducer
+}
+
+func (s *Server) Run(addr string) error {
+	httpServer := &http.Server {
+		Addr: addr,
+		Handler: s.Handler(),
+	}
+	log.Printf("Listening for req on %s...'\n", addr)
+	return httpServer.ListenAndServe()
 }
 
 func (s *Server) Close() error {
@@ -75,9 +92,9 @@ func (s *Server) collectQueryStringData() http.Handler{
 		// no message key -> randomly distributed over the parts.
 		partition, offset, err := s.DataCollector.SendMessage(
 			&sarama.ProducerMessage{
-				Topic: *topic,
+				Topic: "important",
 				Value: sarama.StringEncoder(r.URL.RawQuery),
-				})
+			})
 
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
@@ -114,4 +131,55 @@ func (ale *accessLogEntry) Length() int {
 func (ale *accessLogEntry) Encode() ([]byte, error) {
 	ale.ensureEncoded()
 	return ale.encoded, ale.err
+}
+
+func (s *Server) withAccessLog(next http.Handler) http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		started := time.Now()
+		next.ServeHTTP(w, r)
+		entry := &accessLogEntry{
+			Method:		r.Method,
+			Host:		r.Host,
+			Path:		r.RequestURI,
+			IP:			r.RemoteAddr,
+			RespTime:	float64(time.Since(started)) / float64(time.Second),
+		}
+
+		s.AccessLogProducer.Input() <- &sarama.ProducerMessage{
+			Topic: "access_log",
+			Key:	sarama.StringEncoder(r.RemoteAddr),
+			Value: entry,
+		}
+	})
+}
+
+func newDataCollector(brokerList []string) sarama.SyncProducer {
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+	config.Producer.Return.Successes = true
+
+	producer, err := sarama.NewSyncProducer(brokerList, config)
+	if err != nil {
+		log.Fatalln("Failed to start Sarama producer:", err)
+	}
+	return producer
+}
+
+func newAccessLogProducer(brokerList []string) sarama.AsyncProducer{
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForLocal
+	config.Producer.Compression = sarama.CompressionSnappy
+	config.Producer.Flush.Frequency = 500 * time.Millisecond
+
+	producer, err := sarama.NewAsyncProducer(brokerList, config)
+	if err != nil {
+		log.Fatalln("Failed to start producer:", err)
+	}
+	go func() {
+		for err := range producer.Errors() {
+			log.Println("Failed to write access log entry:", err)
+		}
+	}()
+	return producer
 }
