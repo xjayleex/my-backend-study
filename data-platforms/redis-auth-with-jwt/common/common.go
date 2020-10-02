5b812f633c78b280158cbc159d0aad5ebbf44a65
xjayleex <aprilmagnificat@gmail.com>
2020-10-02 15:58:38 +0900

    Changed some directory structures

---
+++ data-platforms/redis-auth-with-jwt/common/common.go
@@ -0,0 +1,29 @@
+package common
+
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	Username			string
+	HashedPassword		string
+}
+
+func NewUser(username string, password string) (*User, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("cannot hash pw: %w", err)
+	}
+
+	user := &User {
+		Username: username,
+		HashedPassword: string(hashedPassword),
+	}
+	return user, nil
+}
+
+func (user *User) IsCorrectPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(user.HashedPassword), []byte(password))
+	return err == nil
+}
---
+++ data-platforms/redis-auth-with-jwt/rdsclient.go
@@ -0,0 +1,138 @@
+package main
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/go-redis/redis"
+	"github.com/pkg/errors"
+	"os"
+)
+type RedisClient struct {
+	client		*redis.Client
+	address 	string
+	port		string
+	db			int
+	ctx 		context.Context
+}
+
+type RData struct {
+	Key string
+	Value *RValue
+}
+
+type RValue struct {
+	Password string	`json:"password"`
+	Mail	string `json:"mail"`
+}
+
+func (rd *RData) MarshalBinary() ([]byte, error) {
+	return json.Marshal(rd)
+}
+
+func (rd *RData) UnmarshalBinary(data []byte) error {
+	return json.Unmarshal(data,rd)
+}
+
+func (rv *RValue) MarshalBinary() ([]byte, error) {
+	return json.Marshal(rv)
+}
+
+func (rv *RValue) UnmarshalBinary(data []byte) error {
+	return json.Unmarshal(data,rv)
+}
+
+func (rv *RValue) String() string {
+	return fmt.Sprintf("%s %s",rv.Password,rv.Mail)
+}
+
+type RedisClientOpts struct {
+	Address	string
+	Port	string
+	DB 		int
+}
+func NewRedisClient(opts *RedisClientOpts) (c *RedisClient, err error) {
+	if c == nil {
+		c = &RedisClient{}
+	}
+	if c.ctx == nil {
+		c.ctx = context.Background()
+	}
+
+	if opts.Address == ""  {
+		err = errors.Errorf("Address Required")
+		return
+	}
+	if opts.Port == "" {
+		err = errors.Errorf("Port Required")
+	}
+	c.address, c.port, c.db = opts.Address, opts.Port, opts.DB
+	c.client = redis.NewClient(&redis.Options{
+		Addr: c.address + ":" + c.port,
+		DB: c.db,
+	})
+	return
+}
+
+
+func (rc *RedisClient) SetNX(data *RData) {
+	boolCmd := rc.client.SetNX(rc.ctx, data.Key, data.Value,0)
+	flag , err := boolCmd.Result()
+	trap(err)
+	if !flag {
+		fmt.Println("Key Exists Already.")
+	} else {
+		fmt.Println("Success on creating key.")
+	}
+}
+
+func (rc *RedisClient) Get(key string) error {
+	val, err := rc.client.Get(rc.ctx, key).Result()
+	if err != nil {
+		fmt.Println("Key doesn't exist.")
+		return err
+	}
+	fmt.Println(val)
+	return nil
+}
+
+func (rc *RedisClient) GetBytes(key string) (*RValue,error) {
+	val, err := rc.client.Get(rc.ctx, key).Bytes()
+	if err != nil {
+		fmt.Println("Key doesn't exist.")
+		return nil ,err
+	}
+	rv := &RValue{}
+	json.Unmarshal(val,rv)
+	return rv, nil
+}
+
+func main() {
+	rc, err := NewRedisClient(&RedisClientOpts{
+		Address: "localhost",
+		Port: "6379",
+		DB: 1,
+	})
+	trap(err)
+	pong, err := rc.client.Ping(rc.ctx).Result()
+	trap(err)
+	if pong != "PONG" {
+		fmt.Println("Not Connected.")
+		os.Exit(1)
+	}
+	rc.SetNX(&RData{"users:jaehyunlee",
+		&RValue{"password", "bigdata304@gmail.com" }})
+	rc.Get("users:jaehyunlee")
+	rv, err := rc.GetBytes("users:jaehyunlee")
+	trap(err)
+	fmt.Println(rv.String())
+
+}
+
+func trap (err error) {
+	if err == nil {
+		return
+	} else {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
