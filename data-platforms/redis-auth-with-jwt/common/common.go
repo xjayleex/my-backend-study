09ca2c94be76051af8cab282f0d3c627b73eddd5
xjayleex <aprilmagnificat@gmail.com>
2020-10-03 16:42:48 +0900

    writing user sign up logic

--- data-platforms/redis-auth-with-jwt/common/common.go
+++ data-platforms/redis-auth-with-jwt/common/common.go
@@ -2,12 +2,24 @@ package common
 
 import (
 	"fmt"
+	"github.com/pkg/errors"
 	"golang.org/x/crypto/bcrypt"
+	"os"
+	"sync"
+)
+
+var (
+	ErrKeyNotExists = errors.New("Key Not exist.")
+	ErrNotConnected = errors.New("Not Connected.")
+	ErrNilContext = errors.New("Context is nil value.")
+	ErrNoAddress = errors.New("Address Required.")
+	ErrNoPort = errors.New("Port Required.")
 )
 
 type User struct {
 	Username			string
 	HashedPassword		string
+	Mail				string
 }
 
 func NewUser(username string, password string) (*User, error) {
@@ -26,4 +38,40 @@ func NewUser(username string, password string) (*User, error) {
 func (user *User) IsCorrectPassword(password string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(user.HashedPassword), []byte(password))
 	return err == nil
+}
+
+type UserStore interface {
+	Save(user *User)
+	Find(username string) (*User, error)
+}
+
+type RedisUserStore	struct {
+	mutex       sync.RWMutex
+	userInRedis *RedisClient
+}
+
+func NewRedisUserStore(opts *RedisClientOpts) *RedisUserStore {
+	if opts == nil {
+		fmt.Println("Redis Options Needed.")
+		os.Exit(1)
+	}
+	return &RedisUserStore{
+		userInRedis: NewRedisClient(opts),
+	}
+}
+
+func (store *RedisUserStore) Find (username string) (*User, error ) {
+}
+
+
+func (store *RedisUserStore) Save (user *User ) error {
+	store.mutex.Lock()
+	defer store.mutex.Unlock()
+	cmd, err := store.userInRedis.Get(user.Username)
+	// err != nil -> key does not exist -> save 가능
+	// err == nil -> save 불가능
+	if err != nil {
+		fmt.Println(err)
+	}
+
 }
