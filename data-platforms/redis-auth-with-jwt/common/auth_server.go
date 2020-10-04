60eed548856ae6833997034e47e2077a8edfbf9c
xjayleex <aprilmagnificat@gmail.com>
2020-10-04 23:43:43 +0900

    Writing Auth server

---
+++ data-platforms/redis-auth-with-jwt/common/auth_server.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	pb "github.com/xjayleex/idl/protos/auth"
+	"golang.org/x/net/context"
+)
+
+
+type AuthServer struct {
+	userStore UserStore
+	jwtManager *JWTManager
+}
+
+func NewAuthServer (userStore UserStore, jwtManager *JWTManager ) *AuthServer {
+	return &AuthServer{
+		userStore: userStore,
+		jwtManager: jwtManager,
+	}
+}
+
+func (authServer *AuthServer) SignUp (ctx context.Context, req *pb.SignUpRequest) (*pb.SignUpResponse, error)  {
+	if authServer == nil {
+		return nil, ErrNoAuthServer
+	}
+	user, err := NewUser(req.Mail, req.Username, req.Password)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	err = authServer.userStore.SignUp(user)
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	return &pb.SignUpResponse{}, nil
+}
+
+func (authServer *AuthServer) SignIn (ctx context.Context, req *pb.SignInRequest) (*pb.SignInResponse, error) {
+	user, err := authServer.userStore.Find(req.GetMail())
+	if err != nil {
+		return nil, err
+	}
+	if user == nil || !user.IsCorrectPassword(req.GetPassword()) {
+		return nil, ErrIncorrectInfo
+	}
+	token, err := authServer.jwtManager.Generate()
+}
--- data-platforms/redis-auth-with-jwt/common/common.go
+++ data-platforms/redis-auth-with-jwt/common/common.go
@@ -3,10 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/pkg/errors"
 	"golang.org/x/crypto/bcrypt"
 	"os"
 	"sync"
+	"time"
 )
 var (
 	ErrKeyNotExists = errors.New("Key Not exist.")
@@ -16,13 +18,23 @@ var (
 	ErrNoAddress = errors.New("Address Required.")
 	ErrNoPort = errors.New("Port Required.")
 	ErrUnknown = errors.New("Unknown error occurs")
+	ErrUnexpectedToken = errors.New("Unexpected token signing method.")
+	ErrInvalidClaims = errors.New("Invalid token claims")
+	ErrPasswordHash = errors.New("cannot hash pw")
+	ErrNilUserObject = errors.New("Nil user error")
+	ErrNoAuthServer = errors.New("No Auth server")
+	ErrIncorrectInfo = errors.New("Incorrect mail or password")
 )
 
+const (
+	userStoreDB 	= 1
+	tokenStoreDB	= 2
+)
 
 type User struct {
+	Mail				string
 	Username			string
 	HashedPassword		string
-	Mail				string
 }
 
 
@@ -34,27 +46,28 @@ func (u *User) UnmarshalBinary(data []byte) error {
 	return json.Unmarshal(data, u)
 }
 
-func NewUser(username string, password string) (*User, error) {
+func NewUser(mail string, username string, password string) (*User, error) {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		return nil, fmt.Errorf("cannot hash pw: %w", err)
+		return nil, ErrPasswordHash
 	}
 
 	user := &User {
 		Username: username,
+		Mail: mail,
 		HashedPassword: string(hashedPassword),
 	}
 	return user, nil
 }
 
-func (user *User) IsCorrectPassword(password string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(user.HashedPassword), []byte(password))
+func (u *User) IsCorrectPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.HashedPassword), []byte(password))
 	return err == nil
 }
 
 type UserStore interface {
 	SignUp(user *User) error
-	Find(username string) (*User, error)
+	Find(mail string) (*User, error)
 }
 
 type RedisUserStore	struct {
@@ -72,7 +85,16 @@ func NewRedisUserStore(opts *RedisClientOpts) *RedisUserStore {
 	}
 }
 
-func (store *RedisUserStore) Find (username string) (*User, error ) {
+func (store *RedisUserStore) Find (mail string) (*User, error ) {
+	store.mutex.RLock()
+	defer store.mutex.RUnlock()
+	user, err := store.userInRedis.Get(mail)
+	if err != nil {
+		return nil, err
+	}
+	if user == nil {
+		return nil, ErrNilUserObject
+	}
 	return nil, nil
 }
 
@@ -81,17 +103,65 @@ func (store *RedisUserStore) SignUp (user *User ) error {
 	store.mutex.Lock()
 	defer store.mutex.Unlock()
 	err := store.userInRedis.SetNX(&RData{
-		Key:   user.Username,
+		Key:   user.Mail,
 		Value: user,
 	})
 	return err
 }
 
+type JWTManager struct {
+	secretKey		string
+	tokenDuration	time.Duration
+}
+
+type UserClaims struct {
+	jwt.StandardClaims
+	Username string `json:"username"`
+}
+
+func NewJWTManager(secretKey string, tokenDuration time.Duration) *JWTManager{
+	return &JWTManager{secretKey: secretKey, tokenDuration: tokenDuration}
+}
+
+func (manager *JWTManager) Generate(user *User) (string, error) {
+	claims := UserClaims{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(manager.tokenDuration).Unix(),
+		},
+		Username: user.Username,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,claims)
+	return token.SignedString([]byte(manager.secretKey))
+}
+
+func (manager *JWTManager) Verify(accessToken string) (*UserClaims, error) {
+	token, err := jwt.ParseWithClaims(
+		accessToken,
+		&UserClaims{},
+		func(token *jwt.Token) (interface{}, error){
+			_, ok := token.Method.(*jwt.SigningMethodHMAC)
+			if !ok {
+				return nil, ErrUnexpectedToken
+			}
+			return []byte(manager.secretKey), nil
+		})
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(*UserClaims)
+	if !ok {
+		return nil, ErrInvalidClaims
+	}
+	return claims, nil
+}
+/*
 func main() {
 	rs := NewRedisUserStore(&RedisClientOpts{
 		Address: "localhost",
 		Port:    "6379",
-		DB:      1,
+		DB:      userStoreDB,
 	})
 
 	err := rs.SignUp(&User{
@@ -100,4 +170,4 @@ func main() {
 		Mail:           "bigdata304@gmail.com",
 	})
 	fmt.Println(err)
-}
+}*/
